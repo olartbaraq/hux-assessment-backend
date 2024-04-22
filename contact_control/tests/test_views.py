92a5be59cf4f8d325a0cc40d6619dc5f9136d131
mubaraq akanbi <akanbi.ges201@gmail.com>
2024-04-22 15:16:27 +0100

    feat: implemented all crud test for contact control app

--- contact_control/tests/test_views.py
+++ contact_control/tests/test_views.py
@@ -1,10 +1,8 @@
-from rest_framework.test import APITestCase, force_authenticate, APIClient, APIRequestFactory  # type: ignore
+from rest_framework.test import APITestCase, APIClient  # type: ignore
 from django.urls import reverse  # type: ignore
 from rest_framework import status  # type: ignore
-from contact_control.views import ContactViewSet  # type: ignore
 from contact_control.models import Contact
 from django.contrib.auth import get_user_model  # type: ignore
-from user_control.tokenauth import JWTAuthentication
 
 
 User = get_user_model()
@@ -15,36 +13,24 @@ class TestCreateView(APITestCase):
         """
         Hook method for setting up the test fixture before exercising it.
         """
-        self.factory = APIRequestFactory()
         self.client = APIClient()
         self.user = User.objects.create_user(
             email="mubby@test.com",
             password="12345678",
             name="Olart",
         )
-        payload = {"id": self.user.id, "email": self.user.email, "name": self.user.name}
-
-        self.access_token = JWTAuthentication.generate_token(payload=payload)
-
-        # Create a dummy request object
-        dummy_request = self.factory.get("api/v1/contact")
-        self.auth_user = JWTAuthentication.authenticate(self, dummy_request)
-
-        # Set the authenticated user on the request
-        self.request = self.factory.get("api/v1/contact")
-        force_authenticate(self.request, user=self.auth_user[0])
-
-        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + str(self.access_token))
+        self.client.force_authenticate(user=self.user)
+        self.contact_data = {
+            "lastname": "test1",
+            "firstname": "test2",
+            "phone_number": "09081235647",
+        }
 
     def test_create_contact_POST(self):
         """
         simple mockup test to simulate creation of contacts
         """
-        contact_data = {
-            "lastname": "test1",
-            "firstname": "test2",
-            "phone_number": "09081235647",
-        }
+
         contact_data2 = {
             "lastname": "test1",
             "firstname": "test2",
@@ -52,19 +38,18 @@ class TestCreateView(APITestCase):
         }
 
         # Create a request using the factory
-        request = self.factory.post(
+        response = self.client.post(
             reverse("contact-list"),
-            contact_data,
+            self.contact_data,
             format="json",
         )
-
-        # Pass the request to the view
-        response = ContactViewSet.as_view({"post": "create"})(request)
 
         # Assertions
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertIsNotNone(
-            Contact.objects.filter(phone_number=contact_data["phone_number"]).exists()
+            Contact.objects.filter(
+                phone_number=self.contact_data["phone_number"]
+            ).exists()
         )
 
         # Test with client
@@ -73,5 +58,62 @@ class TestCreateView(APITestCase):
         )
         self.assertEqual(client_response.status_code, status.HTTP_201_CREATED)
         self.assertIsNotNone(
-            Contact.objects.filter(phone_number=contact_data["phone_number"]).exists()
+            Contact.objects.filter(phone_number=contact_data2["phone_number"]).exists()
         )
+
+    def test_list_contacts_GET(self):
+        """
+        simple mockup test to simulate getting all contacts
+        """
+        Contact.objects.create(
+            lastname="test1",
+            firstname="test2",
+            phone_number="09081235647",
+        )
+        response = self.client.get(reverse("contact-list"), format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_retrieve_contact_GET(self):
+        contact = Contact.objects.create(
+            lastname="test1",
+            firstname="test2",
+            phone_number="09081235647",
+        )
+        response = self.client.get(
+            reverse(f"contact-detail", kwargs={"pk": contact.id}), format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["lastname"], "test1")
+        self.assertEqual(response.data["firstname"], "test2")
+        self.assertNotEqual(response.data["phone_number"], "12345432123")
+
+    def test_update_contact_PUT(self):
+        contact = Contact.objects.create(
+            lastname="test1", firstname="test2", phone_number="09081235647"
+        )
+        data = {
+            "lastname": "John",
+            "firstname": "Smith",
+            "phone_number": "1234567890",
+        }
+        response = self.client.put(
+            reverse("contact-detail", kwargs={"pk": contact.id}),
+            data=data,
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        updated_contact = Contact.objects.get(pk=contact.id)
+        self.assertEqual(updated_contact.lastname, "John")
+        self.assertEqual(updated_contact.firstname, "Smith")
+        self.assertEqual(updated_contact.phone_number, "1234567890")
+
+    def test_delete_contact_DELETE(self):
+        contact = Contact.objects.create(
+            lastname="test1", firstname="test2", phone_number="09081235647"
+        )
+        response = self.client.delete(
+            reverse("contact-detail", kwargs={"pk": contact.id})
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Contact.objects.count(), 0)
